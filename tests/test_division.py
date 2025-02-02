29a31eba229d289b48eb1fec74eb80acfc6d4671
FantasticApple <zhawhjw@users.noreply.github.com>
2025-02-02 01:42:42 -0800

    first tests commit

---
+++ tests/test_division.py
@@ -0,0 +1,7 @@
+import pytest
+def test_division():
+    pass
+
+def test_divide_zero_exception():
+    with pytest.raises(ZeroDivisionError):
+        pass
---
+++ tests/test_subtraction.py
@@ -0,0 +1,2 @@
+def test_subtraction():
+    pass
